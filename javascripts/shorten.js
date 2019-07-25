5347ab94f77b3e3f166a2e7af71703d8268517f3
fuckthegop <fuckthegop@m-austin.com>
2019-07-25 12:56:31 -0700

    Update shorten.js

--- javascripts/shorten.js
+++ javascripts/shorten.js
@@ -1,22 +1,25 @@
 var resultHTML;
 
 $('.btn-shorten').on('click', function(){
-
     var url = $('#url-field').val();
-                                 
-    $.post( "/add",{ url: url }).done(function( data ) {
-        if(data.message.key){
-            var outUrl = "https://fuckthe.gop/" + data.message.key;
-            resultHTML = '<a class="result" id="foo" target="_blank" href="' + outUrl + '">'+ outUrl + '</a>';
-        }
-        else{
-            resultHTML = 'invalid url.'
-        }
-        $('#link').html(resultHTML);
-        $('#link').hide().fadeIn('slow');
-        $('#url-field').focus();
-    });
-    
+    if(url){
+        if(!/^(https?:\/\/)/.test(url)){
+            url = 'http://' + $('#url-field').val();
+            $('#url-field').val(url);
+        }                             
+        $.post( "/add",{ url: url }).done(function( data ) {
+            if(data.message.key){
+                var outUrl = "https://fuckthe.gop/" + data.message.key;
+                resultHTML = '<a class="result" id="foo" target="_blank" href="' + outUrl + '">'+ outUrl + '</a>';
+            }
+            else{
+                resultHTML = 'invalid url.'
+            }
+            $('#link').html(resultHTML);
+            $('#link').hide().fadeIn('slow');
+            $('#url-field').focus();
+        });
+    }    
 });
 
 $("#url-field").keyup(function(event){

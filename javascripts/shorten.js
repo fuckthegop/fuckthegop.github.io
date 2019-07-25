df5dbe572fdf617d27a8b28280a53b6eeec9aca2
fuckthegop <fuckthegop@m-austin.com>
2019-07-25 12:36:46 -0700

    Update shorten.js

--- javascripts/shorten.js
+++ javascripts/shorten.js
@@ -2,25 +2,18 @@ var resultHTML;
 
 $('.btn-shorten').on('click', function(){
 
-    var callback = function(result){
-        if(result.status_code == 200){
-            var data = result.data;    
-            data.url = data.url.replace(/^(http:\/\/)/, 'https://');
-            resultHTML = '<a class="result" id="foo" target="_blank" href="' + data.url + '">'+ data.url + '</a>';
-        }else{
+    var url = encodeURIComponent($('#url-field').val();
+                                 
+    $.post( "/add",{ url: url }).done(function( data ) {
+        if(data.responseJSON.message.key){
+            var outUrl = "https://fuckthe.gop/" + data.responseJSON.message.key;
+            resultHTML = '<a class="result" id="foo" target="_blank" href="' + outUrl + '">'+ outUrl + '</a>';
+        }
+        else{
             resultHTML = 'invalid url.'
         }
-        $('#link').html(resultHTML);
-        $('#link').hide().fadeIn('slow');
-        $('#url-field').focus();
-    };
+    });
     
-    if(!/^(https?:\/\/)/.test($('#url-field').val())){
-        $('#url-field').val('http://' + $('#url-field').val());
-    }
-    
-    $.getJSON("https://api-ssl.bitly.com/v3/shorten?longUrl="+encodeURIComponent($('#url-field').val())+"&domain=fuckthe.gop&access_token=69a54c096240838a7d09a908fc021a5f12fca7e8&callback=?", callback);
-
 });
 
 $("#url-field").keyup(function(event){
